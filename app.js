d6c4a18599d53af1b90b39952da342311e7660b8
Ahmed Zaheer <153592631+ahmed-khan033@users.noreply.github.com>
2024-02-04 22:47:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,115 @@
+/*Question No 1 
+let a = 10 ;
+document.write("Result" ); 
+document.write("<br>");
+document.write("<br>");
+document.write("The value of a is :" , a);
+document.write("<br>");
+document.write("________________________");
+document.write("<br>");
+document.write("<br>");
+document.write("<br>");
+++ a ;  //Unary operator pre increment 
+document.write("The value of ++a is:" , a);
+document.write("<br>");
+document.write("Now the value of a is:" , a);
+document.write("<br>");
+document.write("<br>");
+//Unary operator post increment 
+document.write("The value of a ++ is :"  , a++); 
+document.write("<br>");
+document.write("Now the value of a is :" , a ++);
+document.write("<br>");
+document.write("<br>");
+-- a ; //Unary operator pre decrement
+document.write("The value of --a is:" , a);
+document.write("<br>");
+document.write("Now the value of a is :" , --a);
+document.write("<br>");
+document.write("<br>");
+//Unary operator post decrement 
+document.write("The value of a-- is:" , a--);
+document.write("<br>");
+document.write("Now the value of a is: " , a--);
+
+Question No 2 
+let a = 2;
+let b = 1;
+document.write("a is : = 2");
+document.write("<br>");
+document.write("b is : = 1");
+document.write("<br>");
+document.write("<br>");
+
+console.log("--a is:", --a);
+console.log("--a - --b is:" , --a - --b);
+console.log("--a - --b + ++b" , --a - --b + ++b );
+console.log("--a - --b + ++b + b--" , --a - --b + ++b + b-- );
+document.write("Result is : 3"); 
+
+
+
+
+//question no 3 
+// // Take input from the user
+// var userName = prompt("Enter your name:");
+
+// // Check if the user entered a name
+// if (userName) {
+//     // Display a personalized greeting
+//     alert("Hello, " + userName + "! Welcome to my Web page.");
+// } else {
+//     // Display a generic greeting if no name is entered
+//     alert("Hello! Welcome to our website.");
+// }
+
+  
+
+
+
+//question no 4
+// // Take input from the user
+// var userInput = prompt("Enter a number for the multiplication table:");
+
+// // Convert user input to a number, or use 5 as default
+// var number = userInput ? parseInt(userInput) : 5;
+
+// // Display the multiplication table in the browser
+// document.write("<h2>Multiplication Table of " + number + "</h2>");
+// for (var i = 1; i <= 10; i++) {
+//     document.write(number + " x " + i + " = " + (number * i) + "<br>");
+// }
+
+
+
+
+
+//question 5
+
+// a) Take three subjects' names from the user and store them in 3 different variables.
+// var subject1 = prompt("Enter the name of the first subject:");
+// var subject2 = prompt("Enter the name of the second subject:");
+// var subject3 = prompt("Enter the name of the third subject:");
+
+// // b) Total marks for each subject is 100, store it in another variable.
+// var totalMarks = 100;
+
+// // c) Take obtained marks for the first subject from the user and store it in a different variable.
+// var obtainedMarksSubject1 = parseFloat(prompt("Enter obtained marks for " + subject1 + ":"));
+
+// // d) Take obtained marks for the remaining 2 subjects from the user and store them in variables.
+// var obtainedMarksSubject2 = parseFloat(prompt("Enter obtained marks for " + subject2 + ":"));
+// var obtainedMarksSubject3 = parseFloat(prompt("Enter obtained marks for " + subject3 + ":"));
+
+// // e) Calculate total marks and percentage.
+// var totalObtainedMarks = obtainedMarksSubject1 + obtainedMarksSubject2 + obtainedMarksSubject3;
+// var percentage = (totalObtainedMarks / (totalMarks * 3)) * 100;
+
+// // Display the result in the browser.
+// document.write("Result:");
+// document.write("\nSubject\t\tTotal Marks\t\tObtained Marks");
+// document.write("\n" + subject1 + "\t\t" + totalMarks + "\t\t\t" + obtainedMarksSubject1);
+// document.write("\n" + subject2 + "\t\t" + totalMarks + "\t\t\t" + obtainedMarksSubject2);
+// document.write("\n" + subject3 + "\t\t" + totalMarks + "\t\t\t" + obtainedMarksSubject3);
+// document.write("\n\nTotal Obtained Marks: " + totalObtainedMarks);
+// document.write("\nPercentage: " + percentage.toFixed(2) + "%");
